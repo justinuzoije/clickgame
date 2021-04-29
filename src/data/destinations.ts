230e927632fbdd204f0ebd5614e13391a46926bd
Justin <justinuzoije@gmail.com>
2021-04-29 10:52:15 -0400

    Non functional version in comments for showing monster

--- src/data/destinations.ts
+++ src/data/destinations.ts
@@ -7,7 +7,7 @@ export const destinations = {
       description: 'You approach the entrance to the forest',
       label: 'Forest Level 1',
       connections: [Location.Forest2],
-      safetyLevel: 8,
+      safetyLevel: 100,
       monsters: [Monster.Wolf]
     },
     [Location.Forest2]: {
@@ -15,7 +15,7 @@ export const destinations = {
       description: 'You enter a maze of trees as you step deeper into the forest',
       label: 'Forest Level 2',
       connections: [Location.Forest1, Location.Forest3],
-      safetyLevel: 5,
+      safetyLevel: 2,
       monsters: [Monster.Bear],
     },
     [Location.Forest3]: {
@@ -23,7 +23,7 @@ export const destinations = {
         description: 'You continue through the forest. Does it ever end?',
         label: 'Forest Level 3',
         connections: [Location.Forest2, Location.Forest4],
-        safetyLevel: 4,
+        safetyLevel: 2,
         monsters: [Monster.Wolf, Monster.Bear],
     },
     [Location.Forest4]: {
@@ -31,7 +31,7 @@ export const destinations = {
         description: 'The trees thin out a bit but the air is oppressive here.',
         label: 'Forest Level 4',
         connections: [Location.Forest3],
-        safetyLevel: 2,
+        safetyLevel: 1,
         monsters: [Monster.Wolf, Monster.Bear],
     },
 };
